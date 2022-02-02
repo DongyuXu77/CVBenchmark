85c8cc3a1a8821e801180744d7a7d31f20d8f5d6
DongyuXU77 <Xudongyu@Bytedance.com>
2022-02-02 15:07:51 +0800

    fix some bugs

--- Pytorch/run.py
+++ Pytorch/run.py
@@ -7,59 +7,60 @@ from data import dataloader
 import torch.optim as optim
 
 def train(model, epoch, trainLoader, optimizer, criterion, device):
-    model.train()
-    correct = 0
-    total = 0
-    trainLoss = 0
-    for batch, (inputs, labels) in enumerate(trainLoader):
-	optimizer.zero_grad()
-	inputs, labels = inputs.to(device), labels.to(device)
-	outputs = model(inputs)
-	_, predict = outputs.max(1)
-	correct = correct+predict.eq(labels).sum().item()
-	total = total+inputs.size(0)
-	loss = criterion(outputs, labels)
-	loss.backward()
-	optimizer.step()
-	trainLoss = trainLoss + loss.item()
-	if torch.cuda.current_device()==0:
-		print("[Epoch:{} batch:{}] Accuracy:{:.4f} Loss:{:.4f}".format(epoch+1, batch+1, correct/total, loss/(batch+1)))
-
-def eval(model, epoch, evalLoader, optimizer, device):
-    model.eval()
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for batch, (inputs, labels) in enumerate(evalLoader):
-		inputs, labels = inputs.to(device), labels.to(device)
-		outputs = model(inputs)
-		_, predict = outputs.max(1)
-		correct = correct + predict.eq(labels).sum().item()
-		total = total + inputs.size(0)
-		if torch.cuda.current_device()==0:
-            		print("[Epoch:{} batch:{}] Accuracy:{:.4f}".format(epoch+1, batch+1, correct/total))
-
-def test(model, epoch,testLoader,device):
-    correct = 0
-    total = 0
-    model.eval()
-    with torch.no_grad():
-        for batch, (inputs, labels) in enumerate(testloader):
+	model.train()
+	correct = 0
+	total = 0
+	trainLoss = 0
+	for batch, (inputs, labels) in enumerate(trainLoader):
+		optimizer.zero_grad()
 		inputs, labels = inputs.to(device), labels.to(device)
 		outputs = model(inputs)
 		_, predict = outputs.max(1)
 		correct = correct+predict.eq(labels).sum().item()
 		total = total+inputs.size(0)
-		if toch.cuda.current_device()==0:
-		print("[Epoch:{} batch:{}] Accuracy:{:.4f}".format(epoch+1, batch+1, correct/total))
+		loss = criterion(outputs, labels)
+		loss.backward()
+		optimizer.step()
+		trainLoss = trainLoss + loss.item()
+		if torch.cuda.current_device()==0:
+			print("[Epoch:{} batch:{}] Accuracy:{:.4f} Loss:{:.4f}".format(epoch+1, batch+1, correct/total, loss/(batch+1)))
+
+def eval(model, epoch, evalLoader, optimizer, device):
+	model.eval()
+	correct = 0
+	total = 0
+	with torch.no_grad():
+		for batch, (inputs, labels) in enumerate(evalLoader):
+			inputs, labels = inputs.to(device), labels.to(device)
+			outputs = model(inputs)
+			_, predict = outputs.max(1)
+			correct = correct + predict.eq(labels).sum().item()
+			total = total + inputs.size(0)
+			if torch.cuda.current_device()==0:
+            			print("[Epoch:{} batch:{}] Accuracy:{:.4f}".format(epoch+1, batch+1, correct/total))
+
+def test(model, epoch,testLoader,device):
+	correct = 0
+	total = 0
+	model.eval()
+	with torch.no_grad():
+		for batch, (inputs, labels) in enumerate(testloader):
+			inputs, labels = inputs.to(device), labels.to(device)
+			outputs = model(inputs)
+			_, predict = outputs.max(1)
+			correct = correct+predict.eq(labels).sum().item()
+			total = total+inputs.size(0)
+			if toch.cuda.current_device()==0:
+				print("[Epoch:{} batch:{}] Accuracy:{:.4f}".format(epoch+1, batch+1, correct/total))
 
 if __name__=="__main__":
-    model = vgg()
-    model, device = gpu_set(model)
-    tranloader, testloader = dataloader({'dataset': "ImageNet"})
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
-    for epoch in range(200):
-        train(model, epoch, trainloader, optimizer, criterion, device)
-        # eval(model, epoch, evalloader, optimizer, device)
-        test(model, epoch, testloader, device)
+	model = vgg()
+	device = torch.cuda.current_device()
+	model = gpu_set(model)
+	tranloader, testloader = dataloader({'dataset': "ImageNet"})
+	criterion = nn.CrossEntropyLoss()
+	optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+	for epoch in range(200):
+		train(model, epoch, trainloader, optimizer, criterion, device)
+		# eval(model, epoch, evalloader, optimizer, device)
+		test(model, epoch, testloader, device)
--- Pytorch/utils.py
+++ Pytorch/utils.py
@@ -2,11 +2,11 @@ import torch
 from torch.nn.parallel import DistributedDataParallel as DDP
 
 def gpu_set(model):
-	if torch.cuda.s_available():
+	if torch.cuda.is_available():
 		torch.distributed.init_process_group(backend='nccl')
 		local_rank = torch.distributed.get_rank()
 		torch.cuda.set_device(local_rank)
 		device = torch.device("cuda", local_rank)
 		model.to(device)
 		model = DDP(model, device_ids=[local_rank], output_device=local_rank, find_unused_parameters=True)
-	return model, device 
+	return model 
