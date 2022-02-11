edc39d1c724b605489df870b0937fd6402c1bd96
DongyuXU77 <Xudongyu@Bytedance.com>
2022-02-11 16:21:47 +0800

    update data indentation

--- pyTorch/data.py
+++ pyTorch/data.py
@@ -3,21 +3,26 @@ import torchvision
 from torchvision import datasets, transforms
 
 def dataloader(config):
-    if config['dataset'] == 'ImageNet':
-        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                         std=[0.229, 0.224, 0.225])
-        transforms_train = transforms.Compose([
-            transforms.RandomCrop(224),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            normalize
-        ])
-        transforms_test = transforms.Compose([
-            transforms.ToTensor(),
-            normalize
-        ])
-        trainSet = torchvision.datasets.ImageNet(root='./ImageNet_data', train=True, download=True, transform=transforms_train)
-        testSet = torchvision.datasets.ImageNet(root='./ImageNet_data', train=False, download=True, transform=transforms_test)
-        tranloader = torch.utils.data.DataLoader(trainSet, batch_size=64, shuffle=True, num_workers=32)
-        testloader = torch.utils.data.DataLoader(testSet, batch_size=256, shuffle=False, num_workers=256)
-        return tranloader, testloader
+	if config['dataset'] == 'ImageNet':
+		normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+						std=[0.229, 0.224, 0.225])
+		transforms_train = transforms.Compose([
+		transforms.RandomCrop(224),
+		transforms.RandomHorizontalFlip(),
+		transforms.ToTensor(),
+		normalize
+		])
+       		transforms_test = transforms.Compose([
+		transforms.ToTensor(),
+		normalize
+		])
+      
+		trainSet = torchvision.datasets.ImageNet(root='./ImageNet_data', train=True, download=True, transform=transforms_train)
+      
+		testSet = torchvision.datasets.ImageNet(root='./ImageNet_data', train=False, download=True, transform=transforms_test)
+      
+		tranloader = torch.utils.data.DataLoader(trainSet, batch_size=64, shuffle=True, num_workers=32)
+      
+		testloader = torch.utils.data.DataLoader(testSet, batch_size=256, shuffle=False, num_workers=256)
+      
+		return tranloader, testloader
