2c227c02818743e161dfa13b6c4f1933f8232365
DongyuXU77 <Xudongyu@Bytedance.com>
2022-02-15 18:42:49 +0800

    change implementation of resnet

--- pyTorch/model/resnet.py
+++ pyTorch/model/resnet.py
@@ -59,6 +59,10 @@ class buildingblock(nn.Module):
 			layers.append(nn.BatchNorm2d(self.expansion*planes))
 		return nn.Sequential(*layers)
 
+	@classmethod
+	def _get_class(cls):
+		return cls
+
 class bottleneck(nn.Module):
 	expansion = 4
 
@@ -91,22 +95,29 @@ class bottleneck(nn.Module):
 			layers.append(nn.Conv2d(in_channels=inPlanes, out_channels=self.expansion*planes, kernel_size=1 ,stride=stride))
 			layers.append(nn.BatchNorm2d(self.expansion*planes))
 		return nn.Sequential(*layers)
+	
+	@classmethod
+	def _get_class(cls):
+		return cls
 
+classDict = {'buildingblock':buildingblock._get_class(), 'bottleneck':bottleneck._get_class()}
 
 class ResNet(nn.Module):
-	def __init__(self, block, num_blocks, num_classes):
+	def __init__(self, ResNetConfiguration, num_classes=1000):
 		super(ResNet, self).__init__()
+		self.block = classDict[ResNetConfiguration['block']]
+		self.num_blocks = ResNetConfiguration['num_blocks']
 		self.inPlanes = 64
 
 		self.conv = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7, stride=2, padding=3)
 		self.bn = nn.BatchNorm2d(num_features=64)
 		self.maxPool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-		self.feature1 = self._makeLayers(block, 64, num_blocks[0], stride=1)
-		self.feature2 = self._makeLayers(block, 128, num_blocks[1], stride=2)
-		self.feature3 = self._makeLayers(block, 256, num_blocks[2], stride=2)
-		self.feature4 = self._makeLayers(block, 512, num_blocks[3], stride=2)
+		self.feature1 = self._makeLayers(self.block, 64, self.num_blocks[0], stride=1)
+		self.feature2 = self._makeLayers(self.block, 128, self.num_blocks[1], stride=2)
+		self.feature3 = self._makeLayers(self.block, 256, self.num_blocks[2], stride=2)
+		self.feature4 = self._makeLayers(self.block, 512, self.num_blocks[3], stride=2)
 		self.avgPool = nn.AvgPool2d(kernel_size=7)
-		self.classifier = nn.Linear(in_features=512*block.expansion, out_features=num_classes)
+		self.classifier = nn.Linear(in_features=512*self.block.expansion, out_features=num_classes)
 
 	def forward(self, x):
 		x = self.maxPool(F.relu(self.bn(self.conv(x))))
@@ -128,7 +139,7 @@ class ResNet(nn.Module):
 		return nn.Sequential(*layers)
 
 if __name__=="__main__":
-	model = ResNet(bottleneck, [3, 8, 36, 3], 1000)
+	model = ResNet(ResNetConfiguration['ResNet152'], 1000)
 	device = 'cuda' if torch.cuda.is_available() else 'cpu'
 	model.to(device)
 	summary(model, (3, 224, 224))
