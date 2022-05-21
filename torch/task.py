6d0a9d7c39dd81b5e43c4550f047778451707ec8
DongyuXu77 <xudongyu@bupt.edu.com>
2022-05-21 17:35:31 +0800

    change design pattern to builder[on-going]

--- torch/task.py
+++ torch/task.py
@@ -5,13 +5,22 @@ class trial:
 		self.optimizer = self.get_optimizer()
 		self.lr_schedule = self.get_lr_schedulr()
 		self.criterion = self.get_criterion()
+        self.device = 
 
 	def train():
+        self.model.train()
 		for epoch in range (epochs):
 			self.initialize_metrics()
 			for batch, (input, label) in enumerate self.dataloader['Train']:
-				self.train_step(epoch, batch)
+				self.train_step(epoch, batch, input, label)
 			self.lr_schedule.step()
 
-	def train_step(epoch, batch):
-		self.
+    def train_step(epoch: int, batch: int):
+        input, label = input.to(self.device), label.to(self.device)
+        output = self.model.forward
+        if (batch+1)%batch_log==0:
+            self.log()
+
+    def log(self):
+        for key, value in self.metrics:
+            print(f"{key} : {value}")
